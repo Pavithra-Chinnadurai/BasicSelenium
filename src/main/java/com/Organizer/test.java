6210896f85e829f51226f79c8f504c6f690d3456
Pavithra <pavithrachinnadurai.l@gmail.com>
2023-07-18 08:30:51 +0530

    commit and  push

---
+++ src/main/java/com/Organizer/test.java
@@ -0,0 +1,23 @@
+package com.Organizer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//textarea[@class=\"gLFyf\"]")).sendKeys("birds",Keys.ENTER);
+		
+	}
+
+}
